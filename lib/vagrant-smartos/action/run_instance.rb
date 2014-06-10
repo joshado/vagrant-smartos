83666e0c20bffa8b586d3871fa11fc5c82fb5a3c
Robin Ricard <ricard.robin@gmail.com>
2014-06-10 18:10:15 +0200

    Add resolvers to the json

--- lib/vagrant-smartos/action/run_instance.rb
+++ lib/vagrant-smartos/action/run_instance.rb
@@ -45,7 +45,8 @@ module VagrantPlugins
             "nics" => [nic],
             "customer_metadata" => {
               "user-script" => "useradd -s /usr/bin/bash -m vagrant && passwd -N vagrant && mkdir -p ~vagrant/.ssh && echo 'vagrant ALL=NOPASSWD: ALL' >> /opt/local/etc/sudoers && echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key' >> ~vagrant/.ssh/authorized_keys"
-            }
+            },
+            "resolvers" => env[:machine].provider_config.resolvers
           }
 
           # Launch!
